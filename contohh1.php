d140b4f2aa3e08eebcfba3ec70c20f12149d856a
MNabil <muhammadnabil313@gmail.com>
2022-03-14 16:08:56 +0700

    latihan CI contoh1.php

---
+++ contohh1.php
@@ -0,0 +1,11 @@
+<?php
+class contohh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Testing</h1>";
+        echo"Nama saya siapa ya?
+            saya tinggal dimana
+            ";
+    }
+}
